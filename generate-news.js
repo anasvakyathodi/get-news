2ffb5f23c191a12dbf8caece88c9b467711c2fb1
anasvakyathodi <anasvakyathodi@gmail.com>
2024-04-23 09:48:23 +0530

    feat: add server

--- generate-news.js
+++ generate-news.js
@@ -1,6 +1,9 @@
 const RssParser = require("rss-parser");
 const fs = require("fs");
 const json = require("./feeds.json"); // assuming your JSON file is an array of objects
+const express = require("express");
+const app = express();
+const port = 3000;
 
 const today = new Date().toISOString().split("T")[0];
 let todayFeeds = [];
@@ -52,3 +55,24 @@ async function getTodayNews() {
 }
 
 getTodayNews();
+// Start the Express server and set an interval to run getTodayNews() daily at midnight
+setInterval(() => {
+  getTodayNews(); // Run getTodayNews() every day at midnight
+}, 1000 * 60 * 60 * 24); // Every 24 hours (1 day)
+
+app.get("/today", (req, res) => {
+  const today = new Date().toISOString().substr(0, 10); // Get today's date in YYYY-MM-DD format
+  fs.readFile(`${today}-news.json`, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send({ error: "File not found" });
+    } else {
+      const jsonFeeds = JSON.parse(data);
+      res.json(jsonFeeds);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
